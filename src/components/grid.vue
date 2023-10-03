970027edcbb9b35333cc98bcad5b4386669363c0
LeoA98 <133421687+LeoA98@users.noreply.github.com>
2023-10-03 07:22:54 -0300

    grid

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -1,3 +1,151 @@
 <template>
-    <div>Teste</div>
+    <div class="grid bg-red-300 grid-cols-4">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>3</div>
+    </div>
+
+    <div class="bg-yellow-300 p-6 m-4 rounded-lg">
+        <div class="m-8 grid grid-cols-3">
+            <div class="bg-pink-300 p-2">1</div>
+            <div class="bg-pink-400 p-2">2</div>
+            <div class="bg-pink-300 p-2">3</div>
+            <div class="bg-pink-400 p-2">4</div>
+            <div class="bg-pink-300 p-2">5</div>
+            <div class="bg-pink-400 p-2">6</div>
+            <div class="bg-pink-300 p-2">7</div>
+            <div class="bg-pink-400 p-2">8</div>
+            <div class="bg-pink-300 p-2">9</div>
+        </div>
+    </div>
+
+
+    <div class="grid grid-cols-6 m-2">
+        <div class="bg-pink-300 col-span-1 rounded-l-lg p-2 px-4">
+            <ul>
+                <li>Home</li>
+                <li>Produtos</li>
+                <li>Contatos</li>
+                <li>Blog</li>
+            </ul>
+        </div>
+
+        <div class="col-span-5 bg-blue-400 p-4 rounded-r-lg">
+            <p> maiores quisquam autem, ut inventore non aliquid libero. Corrupti totam fugiat omnis?
+                Accusantium veritatis voluptas amet iste asperiores aperiam nulla dolor expedita repellendus ducimus
+                deserunt dolores numquam provident minus explicabo, ipsam sit, voluptatibus iure aspernatur voluptatum rerum
+                illum? Sunt fuga veritatis quisquam?
+                Explicabo nesciunt cum perspiciatis corrupti harum inventore soluta, commodi saepe magnam autem adipisci
+                beatae voluptatem rem doloribus officiis amet, minima nihil aliquam? Excepturi quidem necessitatibus
+                assumenda ipsum sapiente libero itaque.
+                Nihil amet omnis rerum vero alias. Aliquid, quia fugit! Consectetur esse quae veniam voluptates,
+                consequuntur ex? Odio nam tempora voluptatum aut eius veritatis pariatur numquam minus, non velit aliquid
+                accusantium.
+                Illum esse voluptatem perferendis sunt, veritatis consequatur, necessitatibus illo, ex facilis laborum
+                fugiat adipisci in eum saepe ipsam officia doloremque eveniet officiis incidunt! Perspiciatis, hic! Nostrum
+                animi earum cumque assumenda.</p>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-4 m-2">
+        <div class="col-span-4 bg-red-500">NavBar</div>
+        <div class="col-span-1 bg-red-400">
+            <p>Menu (lado esquerdo)</p>
+        </div>
+
+        <div class="col-span-2 bg-red-300">
+            <h1 class="p-6 font-extrabold">Titulo</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos illo ut facilis fugit vel quia est doloribus
+                exercitationem quo quod laudantium quidem excepturi ex, soluta ab rem at maiores architecto!
+                Natus sapiente maxime ut quibusdam id deserunt voluptatum porro optio? Tenetur quisquam fugit voluptatum
+                veritatis nostrum aut at accusantium, nesciunt a magnam repellat laboriosam eum neque nemo debitis minima
+                qui?
+                Quis suscipit minima illum sunt provident, voluptatibus molestiae numquam exercitationem alias sit magni
+                ullam doloribus totam accusantium! Debitis accusamus, autem cumque, ab neque maiores doloribus culpa alias
+                fugit, expedita dolorum.
+                Sit consequuntur dolores ea iste eveniet tempore totam dolore, officiis perspiciatis quae ratione nobis quod
+                maiores molestiae rerum recusandae quasi, itaque voluptate ab dicta unde repellendus? Facilis maiores beatae
+                voluptatum.
+                Molestias dicta aut ut non quaerat, debitis voluptate ipsam facilis quis harum dolorum, placeat eveniet ex
+                reiciendis! Mollitia, commodi voluptatibus cupiditate incidunt accusantium illum optio laudantium voluptate
+                ab exercitationem sit!
+                Maiores tempore ullam et, hic voluptatem delectus modi ipsa nihil. Quaerat atque incidunt labore sapiente
+                vel tenetur nihil ipsa reprehenderit iusto. Odio tempora excepturi aliquam reiciendis fuga deserunt delectus
+                animi?
+                Recusandae ab minima maiores placeat eos quis asperiores et at cum reprehenderit ut distinctio, error ea
+                aspernatur, vitae, sed magnam optio libero reiciendis culpa perferendis. Vitae aspernatur nostrum tenetur
+                nobis.
+                Accusantium modi magni aspernatur voluptatibus enim sapiente perferendis facere, laborum reprehenderit porro
+                vero, voluptas, sed amet doloribus rerum! Corrupti laborum tempora rem asperiores. Ex, officia velit
+                voluptate quod exercitationem repellendus!
+                Minima reiciendis explicabo mollitia quae incidunt rem illum ducimus iusto, earum error quod aperiam
+                perferendis velit deserunt unde consectetur quos fuga blanditiis impedit harum corporis! Reiciendis delectus
+                corrupti quas a!
+                Nesciunt vel rem eum quod perferendis, optio temporibus voluptatem ad debitis numquam repudiandae ipsa ipsum
+                labore sit enim illum error veniam itaque totam id sunt, earum porro necessitatibus. Esse, explicabo.</p>
+        </div>
+
+            <div class="col-span-1 bg-red-400">
+                <p>Menu (lado direito)</p>
+            </div>
+
+        <div class="col-span-4 bg-red-500">Footer</div>
+
+    </div>
+
+    <div class="grid grid-cols-4 m-2">
+        <div class="col-span-4 bg-red-500">NavBar</div>
+        <div class="col-span-1 bg-red-400">
+            <ul>
+                <li>Home</li>
+                <li>Produtos</li>
+                <li>Contatos</li>
+                <li>Blog</li>
+            </ul>
+        </div>
+
+        <div class="col-span-3 bg-red-200">
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque est necessitatibus numquam. Nesciunt obcaecati tenetur quidem unde, delectus qui itaque nulla earum facere distinctio aliquam expedita nisi repudiandae, enim odio!
+                Delectus fugit, fuga voluptas aliquam quod natus. Quisquam adipisci beatae, eligendi omnis error debitis excepturi molestiae dolores natus repudiandae praesentium placeat, nostrum libero nesciunt illo deleniti est esse dolorum nemo.
+            </p>
+        </div>
+
+    </div>
+
+    <div class="grid grid-rows-4 grid-flow-col m-2">
+        <div class="bg-green-400">1</div>
+        <div class="bg-green-600">2</div>
+        <div class="bg-green-400">3</div>
+        <div class="bg-green-600">4</div>
+        <div class="bg-green-400">5</div>
+        <div class="bg-green-600">6</div>
+        <div class="bg-green-400">7</div>
+        <div class="bg-green-600">8</div>
+
+    </div>
+
+    <!--GAP-->
+
+    <div class="grid grid-cols-4 m-4 gap-2">
+        <div class="bg-purple-400">1</div>
+        <div class="bg-purple-600">2</div>
+        <div class="bg-purple-400">3</div>
+        <div class="bg-purple-600">4</div>
+        <div class="bg-purple-400">5</div>
+        <div class="bg-purple-600">6</div>
+        <div class="bg-purple-400">7</div>
+        <div class="bg-purple-600">8</div>
+    </div>
+
+    <!--Execercício 9-->
+
+    <div class="grid grid-cols-2 m-4 gap-6">
+        <div class="bg-purple-400">1</div>
+        <div class="bg-purple-500">2</div>
+        <div class="bg-purple-600">3</div>
+        <div class="bg-purple-700">4</div>
+    </div>
+
 </template>
